8b33c0ac1185fe70c8e0ea71b98507f992939a5d
Austin <austinwiese.acw@gmail.com>
2021-01-30 14:02:46 -0800

    updates(contact-table-actions): swapped buttons out for dialogs

--- src/components/contact-manager/contact-table-actions/index.vue
+++ src/components/contact-manager/contact-table-actions/index.vue
@@ -2,26 +2,137 @@
   <div class="contact-table-actions">
     <div class="contact-table-actions__button-container">
       <div class="contact-table-actions__button-container-left">
-        <v-btn class="contact-table-actions__button">
-          <v-icon left dark>
-            mdi-pencil
-          </v-icon>
-          Edit
-        </v-btn>
-        <v-btn class="contact-table-actions__button">
-          <v-icon left dark>
-            mdi-delete
-          </v-icon>
-          Delete
-        </v-btn>
+        <v-dialog
+          v-model="dialog"
+          width="500"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="secondary" v-bind="attrs" v-on="on" class="contact-table-actions__button">
+              <v-icon left dark>
+                mdi-pencil
+              </v-icon>
+              Edit
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title class="headline grey lighten-2">
+              Edit
+            </v-card-title>
+
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog
+          v-model="dialog2"
+          width="500"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="secondary" v-bind="attrs" v-on="on" class="contact-table-actions__button">
+              <v-icon left dark>
+                mdi-delete
+              </v-icon>
+              Delete
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title class="headline grey lighten-2">
+              Delete
+            </v-card-title>
+
+            <v-card-text>
+              Are you sure you want to delete this record?
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog2 = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog2 = false"
+              >
+                Yes, Delete
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </div>
       <div class="contact-table-actions__button-container-right">
-        <v-btn class="contact-table-actions__button">
-          <v-icon left dark>
-            mdi-plus
-          </v-icon>
-          Add Contact
-        </v-btn>
+        <v-dialog
+          v-model="dialog3"
+          width="500"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" v-bind="attrs" v-on="on" class="contact-table-actions__button">
+              <v-icon left dark>
+                mdi-plus
+              </v-icon>
+              Add Contact
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title class="headline grey lighten-2">
+              Add Contact
+            </v-card-title>
+
+            <v-card-text>
+              Are you sure you want to delete this record?
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog3 = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog3 = false"
+              >
+                Yes, Delete
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </div>
     </div>
   </div>
@@ -30,6 +141,13 @@
 <script>
 export default {
   name: 'Contact Actions',
+  data() {
+    return {
+      dialog: false,
+      dialog2: false,
+      dialog3: false,
+    }
+  },
 }
 </script>
 
