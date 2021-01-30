a593aaffddcfd18ec5dc93cb0a4999769661f5cd
Austin <austinwiese.acw@gmail.com>
2021-01-30 11:55:02 -0800

    creates: contact-manager-component

---
+++ src/components/contact-manager/contact-actions/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contact-actions">
+    <v-row no-gutters class="text-center">
+      <v-col cols="5">
+        <v-text-field
+          label="Search"
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact Actions',
+}
+</script>
+
+<style src="./index.sass" lang="sass"></style>
--- src/components/home-main.vue
+++ src/components/home-main.vue
@@ -9,6 +9,7 @@
           contain
           height="200"
         /> -->
+        <ContactActions></ContactActions>
       </v-col>
 
       <!-- <v-col class="mb-4">
@@ -93,61 +94,13 @@
 </template>
 
 <script>
+import ContactActions from '@/components/contact-manager/contact-actions/index.vue'
+
 export default {
   name: 'Home Main',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  components: {
+    ContactActions,
+  },
 }
 </script>
 
